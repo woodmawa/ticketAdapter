863e47921cfbdb0bfea6221d4dd3542d114303e5
woodmawa <will.woodman@btinternet.com>
2018-09-19 17:20:55 +0100

    fixed picking up mail uid and password from properties

--- src/main/groovy/com/softwood/request/LineItem.groovy
+++ src/main/groovy/com/softwood/request/LineItem.groovy
@@ -2,9 +2,14 @@ package com.softwood.request
 
 import com.softwood.cmdb.ConfigurationItem
 
+import java.time.LocalDateTime
+
 class LineItem {
     int lineNumber
     ProductOffering portfolioProduct
     ConfigurationItem productInstance       //optional if provide
     int quantity
+    String status
+    LocalDateTime shippingDate
+
 }
